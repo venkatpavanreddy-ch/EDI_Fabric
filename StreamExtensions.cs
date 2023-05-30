3b882b4e79aa2a6637ad2560d4e1dbded632af81
venkatpavanreddy-ch <venkatpavanreddy.ch@gmail.com>
2023-05-30 12:14:05 +0530

    Add files via upload

---
+++ StreamExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace EDIFabric.Common
+{
+    public static class StreamExtensions
+    {
+        public static string LoadToString(this Stream stream)
+        {
+            stream.Position = 0;
+            using (var reader = new StreamReader(stream, Encoding.Default))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
